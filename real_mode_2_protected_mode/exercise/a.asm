cb98a35cdeea647f2f8fb13a320167a6af4a0eed
wolfwzr <wolfwzr.china@gmail.com>
2014-07-12 22:46:03 +0800

    add b.asm

--- real_mode_2_protected_mode/exercise/a.asm
+++ real_mode_2_protected_mode/exercise/a.asm
@@ -1,3 +1,17 @@
+;本程序测试项：
+;   1. $、$$、label的含义
+;   2. section.xxx.start的含义
+;
+;结果：
+;   1. $$表示vstart
+;   2. $表示vstart+当前指令的段内偏移
+;   3. label表示vstart+当前label的段内偏移
+;   4. section.xxx.start表示当前段在文件中的偏移
+;
+;注：
+;   可以调整vstart的值来测试，不申明section时，vstart默认为当前段在文件中的偏移
+;
+
 section code vstart=0x7c00 
 mov ax,label
 mov ax,0x1+label
